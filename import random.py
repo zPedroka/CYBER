b6c7601b99ac69d5ee338ec52d9ebe1a2c011e1a
zPedroka <111468644+zPedroka@users.noreply.github.com>
2023-06-12 07:20:24 -0300

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,52 @@
+import random
+import threading
+import time
+
+class Cofre:
+    def __init__(self):
+        self.senha = random.randint(0, 99999)
+        self.aberto = False
+
+    def verificar_senha(self, tentativa):
+        if tentativa == self.senha:
+            self.aberto = True
+            return True
+        else:
+            return False
+
+class Hacker(threading.Thread):
+    def __init__(self, nome, cofre):
+        threading.Thread.__init__(self)
+        self.nome = nome
+        self.cofre = cofre
+
+    def run(self):
+        tentativa = 0
+        while not self.cofre.verificar_senha(tentativa):
+            tentativa += 1
+            print(f'{self.nome}: Tentativa {tentativa}')
+            time.sleep(0.1)
+        print(f'{self.nome}: Cofre aberto!')
+
+class Policia(threading.Thread):
+    def __init__(self, cofre):
+        threading.Thread.__init__(self)
+        self.cofre = cofre
+
+    def run(self):
+        time.sleep(10)
+        if not self.cofre.aberto:
+            print('A polícia pegou os hackers e eles estão presos!')
+
+cofre = Cofre()
+hacker1 = Hacker('Hacker 1', cofre)
+hacker2 = Hacker('Hacker 2', cofre)
+policia = Policia(cofre)
+
+hacker1.start()
+hacker2.start()
+policia.start()
+
+hacker1.join()
+hacker2.join()
+policia.join()
